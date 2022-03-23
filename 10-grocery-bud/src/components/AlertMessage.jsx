ab678a156c0514152316e682d5321fd1188e530c
daniellourencoedu <daniellourencoedu@gmail.com>
2022-03-23 18:15:46 -0300

    minor fixes alertMessage.jsx

--- 10-grocery-bud/src/components/AlertMessage.jsx
+++ 10-grocery-bud/src/components/AlertMessage.jsx
@@ -5,19 +5,19 @@ const AlertMessage = ({ message }) => {
 		<p
 			className={`input-message ${
 				(message === 0 && "") ||
-				(message == 1 && "show-remove-msg") ||
-				(message == 2 && "show-remove-msg") ||
+				(message === 1 && "show-remove-msg") ||
+				(message === 2 && "show-remove-msg") ||
 				(message === 3 && "show-add-msg") ||
 				(message === 4 && "show-add-msg") ||
 				(message === 5 && "show-remove-msg")
 			}`}
 		>
 			{(message == 0 && "") ||
-				(message == 1 && "Item removed from the List") ||
-				(message == 2 && "The List is empty") ||
-				(message == 3 && "Item value updated") ||
-				(message == 4 && "Item added to the List") ||
-				(message == 5 && "Please, enter value")}
+				(message === 1 && "Item removed from the List") ||
+				(message === 2 && "The List is empty") ||
+				(message === 3 && "Item value updated") ||
+				(message === 4 && "Item added to the List") ||
+				(message === 5 && "Please, enter value")}
 		</p>
 	);
 };
