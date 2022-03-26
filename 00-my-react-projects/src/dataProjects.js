c67c6f9995fa22c15f24d211aba2272564d3f79b
daniellourencoedu <daniellourencoedu@gmail.com>
2022-03-26 02:45:26 -0300

    update links

--- 00-my-react-projects/src/dataProjects.js
+++ 00-my-react-projects/src/dataProjects.js
@@ -3,56 +3,67 @@ export const dataProjects = [
 		img: "../images/01-reminder.png",
 		title: "Birthday Reminder",
 		url: "https://birthday-reminder-react-project-01.netlify.app/",
+		urlGitgub: "",
 	},
 	{
 		img: "../images/02-tours.png",
 		title: "Tours",
 		url: "https://tours-react-project-02.netlify.app/",
+		urlGitgub: "",
 	},
 	{
 		img: "../images/03-reviews.png",
 		title: "Reviews",
 		url: "https://reviews-react-project-03.netlify.app/",
+		urlGitgub: "",
 	},
 	{
 		img: "../images/04-accordion.png",
 		title: "Accordion",
 		url: "https://accordion-react-project-04.netlify.app/",
+		urlGitgub: "",
 	},
 	{
 		img: "../images/05-menu.png",
 		title: "Menu",
 		url: "https://menu-react-project-05.netlify.app/",
+		urlGitgub: "",
 	},
 	{
 		img: "../images/06-menu-tabs.png",
 		title: "Menu Tabs",
 		url: "https://menu-tabs-react-project-06.netlify.app/",
+		urlGitgub: "",
 	},
 	{
 		img: "../images/07-slider.png",
 		title: "Slider",
 		url: "https://slider-react-project-07.netlify.app/",
+		urlGitgub: "",
 	},
 	{
 		img: "../images/08-ipsum-lorem.png",
 		title: "Lorem Ipsum Generator",
 		url: "https://ipsum-lorem-react-project-08.netlify.app/",
+		urlGitgub: "",
 	},
 	{
 		img: "../images/09-color-generator.png",
 		title: "Color Generator",
 		url: "https://color-generator-react-project-09.netlify.app/",
+		urlGitgub: "",
 	},
 	{
 		img: "../images/10-grocery-bud.png",
 		title: "Grocery Bud",
 		url: "https://grocery-bud-react-project-10.netlify.app/",
+		urlGitgub: "",
 	},
 	{
 		img: "../images/11-responsive-navbar.png",
 		title: "Responsive Navbar",
 		url: "https://navbar01-react-project-11.netlify.app/",
+		urlGitgub: "",
 	},
 ];
 export const dataProjectsWebPages = [
@@ -60,10 +71,12 @@ export const dataProjectsWebPages = [
 		img: "../images/webpage-project-01.png",
 		title: "React Projects Porfolio",
 		url: "https://react-projects-main.netlify.app/",
+		urlGitgub: "",
 	},
 	{
 		img: "../images/webpage-project-02.png",
 		title: "JavaScript Projects Porfolio",
 		url: "https://vanilla-land-js-projects.netlify.app/",
+		urlGitgub: "",
 	},
 ];
