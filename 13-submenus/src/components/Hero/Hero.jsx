d5158be3d5acb3513cf6ec97e26d25b3e373d4de
daniellourencoedu <daniellourencoedu@gmail.com>
2022-03-31 14:53:36 -0300

    hero width fix

--- 13-submenus/src/components/Hero/Hero.jsx
+++ 13-submenus/src/components/Hero/Hero.jsx
@@ -6,20 +6,22 @@ const Hero = () => {
 	const { submenuOpen } = useGlobalContext();
 	return (
 		<div className="hero" onMouseOver={() => submenuOpen(false)}>
-			<div className="hero-bg"></div>
-			<div className="hero-left-side">
-				<h1 className="hero-title">Hero Title or small Description </h1>
-				<p className="hero-text">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, quod
-					adipisci, accusantium sit necessitatibus reprehenderit ut quas eum
-					nostrum provident voluptatibus? Itaque vel suscipit dignissimos
-					debitis impedit expedita, cum quasi?
-				</p>
-				<button className="hero-btn">Click Me</button>
-			</div>
-			<div className="hero-right-side">
-				<div className="hero-img">
-					<p>Some fancy Hero image goes here</p>
+			<div className="section-center">
+				<div className="hero-bg"></div>
+				<div className="hero-left-side">
+					<h1 className="hero-title">Hero Title or small Description </h1>
+					<p className="hero-text">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, quod
+						adipisci, accusantium sit necessitatibus reprehenderit ut quas eum
+						nostrum provident voluptatibus? Itaque vel suscipit dignissimos
+						debitis impedit expedita, cum quasi?
+					</p>
+					<button className="hero-btn">Click Me</button>
+				</div>
+				<div className="hero-right-side">
+					<div className="hero-img">
+						<p>Some fancy Hero image goes here</p>
+					</div>
 				</div>
 			</div>
 		</div>
