e50f75118a8d5166ed1b88c213ddaf8e3421318b
daniellourencoedu <daniellourencoedu@gmail.com>
2022-04-14 15:56:43 -0300

    error page added

--- 15-cocktails/src/App.js
+++ 15-cocktails/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ContextProvider from "./context";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Error from "./pages/Error";
 import { MainSharedLayout } from "./components/SharedLayouts/MainSharedLayout";
 import SingleCocktail from "./pages/SingleCocktail";
 
@@ -17,6 +18,7 @@ function App() {
 							element={<SingleCocktail />}
 						></Route>
 						<Route path="about" element={<About />}></Route>
+						<Route path="*" element={<Error />}></Route>
 					</Route>
 				</Routes>
 			</BrowserRouter>
