42bee8c3b859e533140aec677df2aadbdd3f8ace
unknown <daniellourencoedu@gmail.com>
2023-08-10 17:33:25 +0200

    quizzes app vue project

--- 00-my-react-projects/src/dataProjectsVue.js
+++ 00-my-react-projects/src/dataProjectsVue.js
@@ -16,6 +16,13 @@ export const dataProjectsWebPages = [
 			"https://github.com/DanielLourencoFil/Vue-Projects/tree/main/dev_quizzes",
 	},
 	{
+		img: "../images/notes-vue-app.png",
+		title: "Todos App (Vue)",
+		url: "https://notesvue-app.netlify.app/",
+		urlGithub:
+			"https://github.com/DanielLourencoFil/Vue-Projects/tree/main/notes_vue",
+	},
+	{
 		img: "../images/todos-vue-app.png",
 		title: "Todos App (Vue)",
 		url: "https://todos-vue-app.netlify.app/",
